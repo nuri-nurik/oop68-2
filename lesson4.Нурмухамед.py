8c1d4d02b0d0f548e440903d305ebf49c2fa0f7f
nuri-nurik <Your-emailnuripokopk@gmail.com>
2026-07-20 23:16:16 +0600

    test1

---
+++ lesson4.Нурмухамед.py
@@ -0,0 +1,51 @@
+rates = {
+   "KGS": 1,
+   "USD": 89,
+   "EUR": 96,
+   "RUB": 1.2
+}
+
+class Money:
+    def __init__(self, amount, currency):
+        self.amount = amount
+        self.currency = currency
+
+    def convert_to_kgs(self):
+        return self.amount * rates[self.currency]
+
+    def __str__(self):
+        return f"{self.amount} {self.currency}"
+
+    def __add__(self, other):
+        total_kgs = self.convert_to_kgs() + other.convert_to_kgs()
+        return Money(total_kgs, "KGS")
+
+    def __sub__(self, other):
+        total_kgs = self.convert_to_kgs() - other.convert_to_kgs()
+        return Money(total_kgs, "KGS")
+
+    def __mul__(self, number):
+        return Money(self.amount * number, self.currency)
+
+    def __truediv__(self, number):
+        return Money(self.amount / number, self.currency)
+
+
+money1 = Money(100, "USD")
+money2 = Money(5000, "KGS")
+
+result_add = money1 + money2
+print(f"Сложение: {result_add}")
+
+result_sub = money1 - money2
+print(f"Вычитание: {result_sub}")
+
+result_mul = money1 * 3
+print(f"Умножение: {result_mul}")
+
+result_div = money1 / 2
+print(f"Деление: {result_div}")
+
+
+
+
